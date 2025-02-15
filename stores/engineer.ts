58446c41ff697e30da6cad03a8deb39ebc610a53
KaianDev <kaianvasconcelos@gmail.com>
2025-02-15 14:19:55 -0300

    feat: add zustand for state management and engineer result calculations

---
+++ stores/engineer.ts
@@ -0,0 +1,55 @@
+import { create } from "zustand"
+import { EngineerResultCalc } from "../types/engineer-result-calc"
+
+interface StateProps {
+  results: EngineerResultCalc | null
+}
+
+interface ActionProps {
+  setResults: ({
+    budgetValue,
+    proposalValue,
+  }: {
+    proposalValue: number
+    budgetValue: number
+  }) => void
+  clearResults: () => void
+}
+
+const initialState: StateProps = {
+  results: null,
+}
+
+export const useEngineerStore = create<StateProps & ActionProps>()((set) => ({
+  ...initialState,
+  setResults: ({ budgetValue, proposalValue }) => {
+    const unfeasiblePercentage = 0.75
+    const additionalGuaranteePercentage = 0.85
+    const totalInsurancePercentage = 0.05
+
+    return set(() => {
+      const isUnfeasible = proposalValue < budgetValue * unfeasiblePercentage
+      const needAdditionalGuarantee =
+        proposalValue < budgetValue * additionalGuaranteePercentage
+      const additionalGuarantee =
+        budgetValue * additionalGuaranteePercentage - proposalValue
+      const fivePercentProposalValue = proposalValue * totalInsurancePercentage
+      const totalInsuranceValue = needAdditionalGuarantee
+        ? additionalGuarantee + fivePercentProposalValue
+        : fivePercentProposalValue
+
+      return {
+        results: {
+          budgetValue,
+          proposalValue,
+          isUnfeasible,
+          needAdditionalGuarantee,
+          additionalGuarantee,
+          fivePercentProposalValue,
+          totalInsuranceValue,
+        },
+      }
+    })
+  },
+  clearResults: () => set({ results: null }),
+}))
---
+++ types/engineer-result-calc.ts
@@ -0,0 +1,9 @@
+export interface EngineerResultCalc {
+  budgetValue: number
+  proposalValue: number
+  isUnfeasible: boolean
+  needAdditionalGuarantee: boolean
+  additionalGuarantee: number
+  fivePercentProposalValue: number
+  totalInsuranceValue: number
+}
